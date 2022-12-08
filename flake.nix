a4573ccd5c93f28eaf3eea8f12b0921bcb0d58a3
pupbrained <mars@pupbrained.xyz>
2022-12-07 22:14:33 -0500

    ?????

--- flake.nix
+++ flake.nix
@@ -27,7 +27,6 @@
           inherit system overlays;
         };
       in rec {
-        defaultPackage.x86_64-linux = fenix.packages.x86_64-linux.minimal.toolchain;
         packages = flake-utils.lib.flattenTree {
           turbo = pkgs.callPackage ./turbo.nix {};
           turbo-tooling = pkgs.rustPlatform.buildRustPackage rec {
@@ -65,40 +64,5 @@
         apps.turbo = flake-utils.lib.mkApp {drv = packages.turbo;};
         defaultApp = apps.turbo;
       }
-    )
-    // {
-      overlay = final: prev: {
-        turbo = final.callPackage ./turbo.nix {};
-        turbo-tooling = nixpkgs.legacyPackages."x86_64-linux".rustPlatform.buildRustPackage rec {
-          pname = "turbo-tooling";
-          version = "1.6.0";
-
-          src = ./.;
-          cargoSha256 = "n2zr8437yYU613/PBkEzg6MBuEAzghPi+lzLTTYbGho=";
-          nativeBuildInputs = [
-            nixpkgs.legacyPackages."x86_64-linux".pkg-config
-            (fenix.minimal.withComponents [
-              "cargo"
-              "clippy"
-              "rust-src"
-              "rustc"
-              "rustfmt"
-            ])
-          ];
-
-          preConfigure = ''
-            export PKG_CONFIG_PATH="${nixpkgs.legacyPackages."x86_64-linux".fontconfig.dev}/lib/pkgconfig:${nixpkgs.legacyPackages."x86_64-linux".freetype.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-          '';
-
-          doCheck = false;
-
-          meta = with nixpkgs.legacyPackages."x86_64-linux".lib; {
-            description = "Incremental bundler and build system optimized for JavaScript and TypeScript, written in Rust – including Turborepo and Turbopack.";
-            homepage = "https://turbo.build/";
-            license = licenses.unlicense;
-            maintainers = [maintainers.tailhook];
-          };
-        };
-      };
-    };
+    );
 }
